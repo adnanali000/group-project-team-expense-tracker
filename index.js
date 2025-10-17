68763a3e9e8ef997e6f361cad63f10a9ef91e12b
hassanali-codes <ha7298624@gmail.com>
2025-10-17 19:03:18 +0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
- let appData = {
-  members: [],
-  expenses: [],
-  nextExpenseId: 1
-};
-
-function saveData(){
+ function saveData(){
     localStorage.setItem("teamData", JSON.stringify(appData))
     console.log("✅ Data saved to localStorage.");
 }
